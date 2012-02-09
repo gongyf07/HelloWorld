808251e115ee74b71291cd7e3970da31b8171225
gongyf <gongyf07@gmail.com>
2012-02-09 17:55:38 +0800

    first

---
+++ src/com/thss/Test1Activity.java
@@ -0,0 +1,13 @@
+package com.thss;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
